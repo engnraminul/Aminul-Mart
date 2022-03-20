74917b5a879cc2860bae6ecea074f5e889641d54
engnraminul <engnraminul@gmail.com>
2022-03-20 12:06:49 +0600

    setup ecommerce template

--- Aminul_Mart/settings.py
+++ Aminul_Mart/settings.py
@@ -110,12 +110,21 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIR = [STATIC_DIR,]
 
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static_root'), ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+# MEDIA_RUL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+
+
+
 
 LOGIN_URL = '/account/signin/'
 
