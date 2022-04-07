954eb0106d45a28d410a453451d6821ed6fcfbbc
engnraminul <engnraminul@gmail.com>
2022-04-07 22:36:50 +0600

    create search system

--- Shop/views.py
+++ Shop/views.py
@@ -1,18 +1,31 @@
 from django.shortcuts import render
 
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, TemplateView
 from Shop.models import Category, Product, ProductImageGallery, Banner
 
 
-class ProductList(ListView):
-    model = Product
-    template_name = 'shop/home.html'
-    context_object_name = 'products'
+class ProductList(TemplateView):
+    def get(self, request, *args, **kwargs):
+        products = Product.objects.all().order_by('-id')
+        banners = Banner.objects.filter(banner_active=True).order_by('-id')[0:2]
 
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-        context['banners'] = Banner.objects.filter(banner_active=True).order_by('-id')[0:2]
-        return context
+        context = {
+            'products':products,
+            'banners':banners,
+        }
+        return render(request, 'Shop/home.html', context)
+
+    def post(self, request, *args, **kwargs):
+         if request.method =='POST' or request.method == 'post':
+             search_product = request.POST.get('search_product')
+             products = Product.objects.filter(product_name__icontains=search_product).order_by('-id')
+
+             context = {
+                'products':products,
+             }
+             return render(request, 'Shop/home.html', context)
+
+
 
 #product details function based
 
